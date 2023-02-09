6459c4d5441c330e3aff27f880d801f4fab28e8e
Jon Allured <jon.allured@gmail.com>
2023-02-09 08:28:29 -0600

    Initial Commit

---
+++ ArtAdvisor/ArtAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAdvisorApp.swift
+//  ArtAdvisor
+//
+//  Created by Jonathan Allured on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
